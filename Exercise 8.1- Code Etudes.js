f4f24989d9ba568f8da5f1f62eafaf6c32d64a86
Jessica Standage <Jessica@Jessicas-MacBook-Pro.local>
2019-05-31 13:13:16 -0700

    Create Exercise 8.1- Code Etudes.js

---
+++ Exercise 8.1- Code Etudes.js
@@ -0,0 +1,38 @@
+<!iDOCTYPE html>
+<html>
+<body>
+
+<h2>Exercise 8.1: Code Etudes</h2>
+<h6>Jessica Standage</h6>
+
+<p id="demo"></p>
+
+<script>
+
+// Constructor function for Person objects
+
+class Character {
+  constructor(name, age, favorite song)
+    function Person(name, age, favorite song) {
+      this.name = name;
+      this.age = age;
+      this.favoritesong = favorite song;
+  }
+}
+    getName() {
+      return this.name + " " + this.age;
+    }
+
+
+// Create two Person objects
+var sayMyName = new Person("Jessica";
+var sayAge = new Person("Standage");
+var sayFavoriteSong = new Person("21");
+
+// Display name
+document.getElementById("demo").innerHTML = "My name is " + sayMyName.name;
+
+</script>
+
+</body>
+</html>
