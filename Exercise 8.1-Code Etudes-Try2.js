a34bbb89c3b7c59e90658242e9c7a0384177abe8
Jessica Standage <Jessica@Jessicas-MacBook-Pro.local>
2019-06-07 13:34:47 -0700

    Exercise 8.1_Try 2

---
+++ Exercise 8.1-Code Etudes-Try2.js
@@ -0,0 +1,21 @@
+<p id="demo"></p>
+
+// Constructor function for Person objects
+
+class Character {
+  constructor(name, age, favorite song){
+      this.name = name;
+      this.age = age;
+      this.favoritesong = favorite song;
+    }
+
+    getName() }
+      return this.name + " " + this.age;
+
+// Create three Person objects
+var sayMyName = new Person("Jessica");
+var sayAge = new Character("21");
+var sayfavorite_song = new Character("Timeless");
+
+// Display name
+document.getElementById("demo").innerHTML = "My name is " + sayMyName.name;
